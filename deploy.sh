b075553141f5e1ecc4eb3d653ec1d3f1a25d78ec
Mike Bayer <mike_mp@zzzcomputing.com>
2019-09-13 12:15:58 -0400

    - dev

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ function dryrun {
 	${ANSIBLE_BIN} \
         -vv -i hosts \
     $1 $2 $3 $4 $5 $6 $7 $8 $9 \
-        site.yml
+        write_apc.yml
 }
 
 function install {
@@ -20,12 +20,20 @@ function install {
         -vv -i hosts \
         --extra-vars "dryrun=false" \
     $1 $2 $3 $4 $5 $6 $7 $8 $9 \
-        site.yml
+        write_apc.yml
+}
+
+function revert {
+    ${ANSIBLE_BIN} \
+        -vv -i hosts \
+        --extra-vars "dryrun=false" \
+    $1 $2 $3 $4 $5 $6 $7 $8 $9 \
+        revert_apc.yml
 }
 
 
 main() {
-	INSTALL_CMDS="dryrun install"
+	INSTALL_CMDS="dryrun install revert"
 
     CMDS="$@"
 
@@ -38,10 +46,13 @@ main() {
         echo -e "\nusage: $0 <commands>\n"
         echo -e "\n dryrun - do a dry run, showing what we're doing but don't change anything"
         echo -e "\n install - run the install for real"
+        echo -e "\n revert - revert the per-controller configurations"
     fi
 
     if [[ "${CMDS}" == *"dryrun"* ]]; then
         dryrun
+    elif [[ "${CMDS}" == *"revert"* ]]; then
+        revert
     elif [[ "${CMDS}" == *"install"* ]]; then
         install
     fi
